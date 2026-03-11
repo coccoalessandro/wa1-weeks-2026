76e455b84c6d12a61fd61c56992031fb7bed5948
COCCO ALESSANDRO s353655 <s353655@studenti.polito.it>
2026-03-11 18:07:05 +0100

    Solved exercise 2

--- week03/exercise2/entities.js
+++ week03/exercise2/entities.js
@@ -1,6 +1,11 @@
 import dayjs from 'dayjs'
 import sqlite from 'sqlite3'
 
+const db = new sqlite.Database("questions.sqlite", (err) => {
+    if (err) console.log("Error in opening the database.")
+    else console.log("Database opened succesfully.")
+})
+
 // Constructor functions matching database tables
 
 function User(id, name, email) {
@@ -10,7 +15,7 @@ function User(id, name, email) {
 }
 
 function Answer(id, text, author, date, score = 0) {
-    thid.id = id;
+    this.id = id;
     this.text = text;
     this.author = author;
     this.date = dayjs(date);
@@ -20,19 +25,67 @@ function Answer(id, text, author, date, score = 0) {
 }
 
 function Question(id, text, author, date) {
+    this.id = id
     this.text = text;
     this.author = author;
     this.date = dayjs(date);
+    
 
-    this.getAnswers = undefined
-    this.addAnswer = undefined
+    this.getAnswers = function () {
+        return new Promise((resolve, reject) => {
+            const sql = "SELECT * FROM answer WHERE questionId == ?"
+            db.all(sql, [this.id], (err, rows) => {
+                if (err) reject(err)
+                else resolve(rows)
+            })
+        })
+    }
+
+    this.addAnswer = function (answer) {
+        return new Promise((resolve, reject) => {
+            const sql = "INSERT INTO answer(id, text, authorId, date, score, questionId) VALUES (?, ?, ?, ?, ?, ?)"
+            db.run(sql, [answer.id, answer.text, answer.author, dayjs(answer.date), answer.score, this.id], (err) => {
+                if (err) reject(err)
+                else resolve()
+            })
+        })
+    }
+
+    this.voteAnswer = function (id, value) {
+        return new Promise((resolve, reject) => {
+            let val = 1
+            if (value == 'down') val = -1
+            const sql = "UPDATE answer SET score = score + ? WHERE id = ?"
+            db.run(sql, [val, id], (err) => {
+                if (err) reject(err)
+                else resolve()
+            })
+        })
+    }
 }
 
 // For managing lists of questions
 
 function QuestionList() {
-    this.addQuestion = undefined
-    this.getQuestion = undefined
+    this.addQuestion = function (question) {
+        return new Promise((resolve, reject) => {
+            const sql = "INSERT INTO question(id, text, authorId, date) VALUES (?, ?, ?, ?)"
+            db.run(sql, [question.id, question.text, question.author, question.date], (err) => {
+                if (err) reject(err)
+                else resolve()
+            })
+        })
+    }
+    
+    this.getQuestion = function (id) {
+        return new Promise((resolve, reject) => {
+            const sql = "SELECT * FROM question WHERE id = ?"
+            db.get(sql, [id], (err, row) => {
+                if (err) reject(err)
+                else resolve(row)
+            })
+        })
+    }
 }
 
 export { User, Question, QuestionList, Answer }
--- week03/exercise2/index.js
+++ week03/exercise2/index.js
@@ -1,1 +1,30 @@
 import { User, Question, QuestionList, Answer } from "./entities.js";
+
+// 1. Test getQuestion
+
+const qList = new QuestionList()
+const q = await qList.getQuestion(1)
+console.log(q)
+
+
+// 2. Test getAnswers
+
+const q1 = new Question(q.id, q.text, q.author, q.date)
+const answers = await q1.getAnswers()
+console.log(answers)
+
+// 3. addQuestion
+
+//const newQuestion = new Question(12, "What's the distance between the Moon and the Earth?", 1, dayjs("2026-03-11"))
+//await qList.addQuestion(newQuestion)
+
+// 4. addAnswer
+
+// const newAnswer = new Answer(7, "I don't know", 3, "2026-03-11", 0)
+// await q1.addAnswer(newAnswer)
+
+
+// 5. voteAnswer
+
+// await q1.voteAnswer(1, 'up')
+await q1.voteAnswer(1, 'down')
